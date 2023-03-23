7a272dc81363d7d3de354083c2d6e9c892644574
filecc <fil.gia@live.it>
2023-03-23 18:30:36 +0100

    section who i am complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        
+        'fg-black': '#030303',
+        'fg-medium-brown': '#7A746E',
+        'fg-light-cream': '#FFF7F0',
+        'fg-galactic-blue': '#755CDE',
+        'fg-summer-yellow': '#F6A560',
+        'fg-pink': '#F39E9E',
+        'fg-light-red': '#EB7565',
+        'fg-cyan': '#61C4B7',
+        'fg-dark-purple': '#552049',
       },
       fontFamily: {
-        'kumbu': 'Kumbh Sans',
+        'jakarta': 'Plus Jakarta Sans',
+      },
+      fontSize: {
+        'fg-headL': '56px',
+        'fg-headM': '40px',
+        'fg-headS': '32px',
+        'fg-body': '18px'
       }
     },
   },
