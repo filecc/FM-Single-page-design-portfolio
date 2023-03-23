6c29771315c865dca545fd379d216af773422449
filecc <fil.gia@live.it>
2023-03-23 22:13:29 +0100

    removed some functions

--- js/index.js
+++ js/index.js
@@ -9,15 +9,6 @@ function addID(element, id) {
   element.setAttribute('id', id);
 }
 
-/* Creates a new HTML element with the specified tag name, classes and text content */
-function createChild(tagName, id, classes, text) {
-  const newElement = document.createElement(tagName);
-  if (classes) addClasses(newElement, classes);
-  if (id) addID(newElement, id);
-  if (text) (newElement.textContent = text);
-  return newElement;
-}
-
 /* Creates a new HTML element with an object in input */
 function createChildFromObject(props) {
   const {
@@ -34,56 +25,6 @@ function createChildFromObject(props) {
   }
   return newElement;
 }
-
-/* Remove the specified classes to the given HTML element */
-function removeClasses(element, classes) {
-  classes.forEach((_class) => element.classList.remove(_class));
-}
-
-/* Toggle the specified classes to the given HTML element */
-function toggleClasses(element, classes) {
-  classes.forEach((_class) => element.classList.toggle(_class));
-}
-
-/* Adds the desired attribute to the given element */
-function addAttributes(element, props) {
-  const { ...rest } = props;
-  Object.entries(rest).forEach(([key, value]) => {
-    element.setAttribute(key, value);
-  });
-}
-
-// function to get a random number, extreme included
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-// function to manage search filter on a list using a event listner input
-// restituisce SOLO keyToMatch passato
-const handleSearch = (
-  // restituisce SOLO keyToMatch passato
-  arrayToFilter,
-  keyToMatch,
-  keyToShow,
-  source,
-  listToUpdate,
-  itemToCreate,
-) => {
-  const searched = arrayToFilter.filter((element) => element[keyToMatch]
-    .toString()
-    .toLowerCase()
-    .includes(source.value.toLowerCase()));
-  const arraySearched = searched.length
-    ? searched.map((item) => createChild(
-      itemToCreate,
-      '',
-      '',
-      `${item[keyToMatch]} - ${item[keyToShow]}`,
-    ))
-    : [createChild(itemToCreate, '', '', 'no result')];
-
-  listToUpdate.replaceChildren(...arraySearched);
-};
 
 
 const slider = document.getElementById('slider');
@@ -154,7 +95,7 @@ function updateSlider() {
   } else if (currentImage === images.length - 1) {
     position = imageWidth * images.length;
   } else {
-    // sto diocane di calcolo
+
     position = (
       (imageWidth * currentImage) + (sliderGap * currentImage)
       - (((sliderWidth - imageWidth) / 2) - (sliderGap * 2))
